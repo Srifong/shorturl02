4282592efc9da8c56664e7cdc979b6a3ef39d258
621998022 <621998022@crru.ac.th>
2023-02-22 17:15:13 +0700

    ok

--- testmongo/testmongo/settings.py
+++ testmongo/testmongo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c@5(_+d+@h9#v^bgt(#t6=m#*c)gp-i73ni-@tz2ez80a@^9_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS =['http://127.0.0.1:8000','http://localhost:8080']
 # ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
@@ -141,9 +141,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'))
-django_heroku.settings(locals())
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'))
+# django_heroku.settings(locals())
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
